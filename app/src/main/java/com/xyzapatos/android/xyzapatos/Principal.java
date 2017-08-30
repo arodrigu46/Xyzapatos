ca93dd870abd5b7800c8b0be2f4d179033215bbd
Andres <arodrigu46@cuc.edu.co>
2017-08-29 19:48:41 -0500

    Interfaz lista

--- app/src/main/java/com/xyzapatos/android/xyzapatos/Principal.java
+++ app/src/main/java/com/xyzapatos/android/xyzapatos/Principal.java
@@ -1,13 +1,29 @@
 package com.xyzapatos.android.xyzapatos;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Principal extends AppCompatActivity {
 
+    private Resources resources;
+    private Spinner spMarca;
+    private String arrayMarca[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+
+        resources  = this.getResources();
+        spMarca = (Spinner)findViewById(R.id.spMarca);
+        arrayMarca = resources.getStringArray(R.array.arrayMarca);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayMarca);
+        spMarca.setAdapter(adapter);
+
+
+
     }
 }
